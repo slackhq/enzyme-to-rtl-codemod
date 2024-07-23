ee9f45c5ee4e25bed27a1b0d7eff5c67c75916d4
Sergii Gorbachov <sgorbachov@slack-corp.com>
2024-07-23 12:13:38 -0700

    Removed child process to call jscodeshift and use direct apis

--- src/utils/ast-transformations/run-ast-transformations.ts
+++ src/utils/ast-transformations/run-ast-transformations.ts
@@ -1,61 +1,33 @@
 import fs from 'fs';
-import * as path from 'path';
-import { runCommand } from '../shell-helper/shell-helper';
+import { mainTransform } from './main-ast-transform';
+import { astLogger } from './utils/ast-logger';
 import {
     getConfigProperty,
     checkConfiguration,
     addPathsToConfig,
 } from '../config';
+
 /**
- * Run jscodeshift and make AST conversion
+ * Run jscodeshift and make AST conversions
  * @param filePath
  * @returns
  */
-export const getASTCodemodCode = async (filePath: string): Promise<string> => {
+export const getASTCodemodCode = (filePath: string): string => {
     // Check if config is set
     checkConfiguration(filePath);
 
     // Add to configuration
     addPathsToConfig(filePath);
 
-    console.log('Running AST codemod');
-
-    // Get jscodeshift path installed in user's project
-    const jscodeshiftPath = path.join(
-        process.cwd(),
-        'node_modules',
-        '.bin',
-        'jscodeshift',
-    );
-
-    // Resolve the path to your transform function
-    const transformPath = require.resolve(
-        'test-package-useless/dist/utils/ast-transformations/main-transform',
-    );
+    // Run main transformation function in jscodeshift
+    astLogger.info('Start: Running AST codemod');
+    const astTransformedCode = mainTransform(filePath);
 
     // Get ast transformed file path
     const astConvertedFilePath = `${getConfigProperty('astTranformedFilePath')}`;
+    astLogger.info(`Writing AST transformed code to ${astConvertedFilePath}`);
+    fs.writeFileSync(astConvertedFilePath, astTransformedCode, 'utf8');
 
-    // Create jscodeshift command
-    const jscodeshiftCommandToConvert = `${jscodeshiftPath} --parser=tsx -t ${transformPath} ${filePath} -d --outputFile ${astConvertedFilePath}`;
-
-    // Run jscodeshift in child process
-    try {
-        await runCommand(jscodeshiftCommandToConvert);
-    } catch (error) {
-        console.log(`Did not work here. Error: ${error}`);
-    }
-
-    // Return output
-    let astCodemodOutput =
-        'Could not convert. Proceed without ast converted file';
-    try {
-        astCodemodOutput = fs.readFileSync(astConvertedFilePath, 'utf-8');
-    } catch (error) {
-        console.log(
-            `Could not open the file. Error in outputting file ${error}`,
-        );
-    }
-    console.log('Running AST codemod: DONE');
-    return astCodemodOutput;
+    astLogger.info('Done: Running AST codemod');
+    return astTransformedCode;
 };

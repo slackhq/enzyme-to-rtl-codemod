4c9d4c9230f0684dbc49d3fbabe04958c83ab8ea
Crescencio Zul <crescencio.zul@slack-corp.com>
2024-07-24 13:32:30 -0400

    type check

--- src/utils/ast-transformations/individual-transformations/convert-enzyme-text-method.ts
+++ src/utils/ast-transformations/individual-transformations/convert-enzyme-text-method.ts
@@ -3,7 +3,12 @@
  * Ex. expect(wrapper.find('selector').text()).toEqual('Expected text'); -> expect(wrapper.find('selector')).toHaveTextContent('Expected text');
  */
 
-import type { Collection, JSCodeshift, CallExpression } from 'jscodeshift';
+import type {
+    Collection,
+    JSCodeshift,
+    CallExpression,
+    MemberExpression,
+} from 'jscodeshift';
 import { astLogger } from '../utils/ast-logger';
 
 /**
@@ -44,27 +49,57 @@ const getAllTextConversion = (
  * @param root - root AST node
  * @returns - Returns toHaveTextContent() method call
  */
-export const convertText = (j: JSCodeshift, root: Collection) => {
+export const convertText = (j: JSCodeshift, root: Collection): void => {
     const textCallExpressions = getAllTextConversion(j, root);
 
     textCallExpressions.replaceWith(
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ({ node }: { node: any }): CallExpression => {
-            const resultIdentifier =
-                node.callee.object.arguments[0].callee.object;
+        ({ node }: { node: CallExpression }): CallExpression => {
+            // First, check if the callee is a MemberExpression
+            if (node.callee.type === 'MemberExpression') {
+                const memberCallee = node.callee as MemberExpression;
 
-            astLogger.verbose(
-                'Transforms toHaveTextContent within the expect assertion',
-            );
-            return j.callExpression(
-                j.memberExpression(
-                    j.callExpression(j.identifier('expect'), [
-                        resultIdentifier,
-                    ]),
-                    j.identifier('toHaveTextContent'),
-                ),
-                [node.arguments[0]],
-            );
+                // Next, check if the memberCallee.object is a CallExpression
+                if (memberCallee.object.type === 'CallExpression') {
+                    const objectCallExpression =
+                        memberCallee.object as CallExpression;
+
+                    // Further, check if the first argument of the object call is itself a CallExpression
+                    if (
+                        objectCallExpression.arguments[0] &&
+                        objectCallExpression.arguments[0].type ===
+                            'CallExpression'
+                    ) {
+                        const firstArgCallExpression = objectCallExpression
+                            .arguments[0] as CallExpression;
+
+                        // Finally, check if the callee of the first argument is a MemberExpression
+                        if (
+                            firstArgCallExpression.callee.type ===
+                            'MemberExpression'
+                        ) {
+                            const innerMemberExpression =
+                                firstArgCallExpression.callee as MemberExpression;
+                            const resultIdentifier =
+                                innerMemberExpression.object; // Safely access the nested object
+
+                            astLogger.verbose(
+                                'Transforms toHaveTextContent within the expect assertion',
+                            );
+                            return j.callExpression(
+                                j.memberExpression(
+                                    j.callExpression(j.identifier('expect'), [
+                                        resultIdentifier,
+                                    ]),
+                                    j.identifier('toHaveTextContent'),
+                                ),
+                                [node.arguments[0]],
+                            );
+                        }
+                    }
+                }
+            }
+            // Return the node unmodified if any conditions fail
+            return node;
         },
     );
 };

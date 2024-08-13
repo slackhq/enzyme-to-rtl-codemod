d2942dfa7ed31496f1d8457c5ac50fd3e18c5244
Sergii Gorbachov <sgorbachov@slack-corp.com>
2024-08-13 16:29:54 -0700

    added shell helper tests

--- src/support/shell-helper/shell-helper.ts
+++ src/support/shell-helper/shell-helper.ts
@@ -38,66 +38,57 @@ export interface ShellProcess {
  * @param command cli command, e.g. any shell command
  * @returns command output
  */
-export const runCommand = async (command: string): Promise<ShellProcess> => {
-    // Start child process
-    const childProcess = child.spawn(command, {
-        shell: true,
-    });
+export const runCommand = (command: string): Promise<ShellProcess> => {
+    return new Promise((resolve, reject) => {
+        // Start child process
+        const childProcess = child.spawn(command, {
+            shell: true,
+        });
 
-    // Set shell object
-    const shell: ShellProcess = {
-        process: childProcess,
-        output: '',
-        stderr: '',
-        finished: false,
-        command,
-    };
+        // Set shell object
+        const shell: ShellProcess = {
+            process: childProcess,
+            output: '',
+            stderr: '',
+            finished: false,
+            command,
+        };
 
-    // Listen to data event that returns all the output and collect it
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    childProcess.stdout.on('data', (data: any) => {
-        shell.output += data.toString();
-    });
+        // Listen to data event that returns all the output and collect it
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        childProcess.stdout.on('data', (data: any) => {
+            shell.output += data.toString();
+        });
 
-    // Collect error output
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    childProcess.stderr.on('data', (data: any) => {
-        shell.stderr += data.toString();
-    });
+        // Collect error output
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        childProcess.stderr.on('data', (data: any) => {
+            console.log('workign....');
+            shell.stderr += data.toString();
+        });
 
-    // Set the finished flag to true on close event
-    childProcess.on('close', () => {
-        shell.finished = true;
-    });
+        // Set the finished flag to true on close event
+        childProcess.on('close', () => {
+            shell.finished = true;
+            resolve(shell);
+            clearTimeout(timeout);
+        });
 
-    // Wait for shell.finished state
-    await checkIfFinished(shell);
+        // Handle errors from the child process
+        childProcess.on('error', (error) => {
+            reject(error);
+        });
 
-    return shell;
-};
-
-/**
- * Logic to wait for child process to finish executing
- * - Check if the close event was emitted, else wait for 5 sec
- * @param shell shell process
- */
-const checkIfFinished = async (shell: ShellProcess): Promise<void | Error> => {
-    const timeout = 5000;
-    const waitingTimeoutTotal = 300000;
-    let waitedFor = 0;
-
-    while (!shell.finished) {
-        // eslint-disable-next-line no-await-in-loop
-        await sleep(timeout);
-        waitedFor += timeout;
-
-        if (waitedFor > waitingTimeoutTotal) {
-            // Kill the process
-            throw new Error(
-                `checkIfFinished\nFailed to finish after ${waitingTimeoutTotal} ms.\nCommand: ${
-                    shell.command
-                }\nCurrent output: \n${shell.output + shell.stderr}`,
+        // Race between process completion and timeout
+        const timeoutTime = 180000;
+        const timeout = setTimeout(() => {
+            console.log('Process took too long, killing it...');
+            shell.process.kill(); // Kill the process
+            reject(
+                new Error(
+                    `Failed to finish after ${timeoutTime / 1000 / 60} minutes`,
+                ),
             );
-        }
-    }
+        }, timeoutTime);
+    });
 };

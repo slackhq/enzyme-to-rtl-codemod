0a8a12d6ceb6213156f64bfb4c27fcef717b1682
Sergii Gorbachov <sgorbachov@slack-corp.com>
2024-08-19 12:29:05 -0700

    added better alerting for jest run logs

--- src/support/enzyme-helper/get-dom-enzyme.jest.ts
+++ src/support/enzyme-helper/get-dom-enzyme.jest.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import * as getDomEnzyme from './get-dom-enzyme';
 import { getConfigProperty } from '../config';
-import { runCommand } from '../shell-helper/shell-helper';
+import { runCommand, ShellProcess } from '../shell-helper/shell-helper';
 
 // Mocks
 jest.mock('fs');
@@ -266,7 +266,7 @@ describe('getReactCompDom', () => {
             .mockReturnValue('');
         const runJestInChildProcessMock = jest
             .spyOn(getDomEnzyme, 'runJestInChildProcess')
-            .mockResolvedValue();
+            .mockResolvedValue('');
         const getDomTreeOutputFromFileMock = jest
             .spyOn(getDomEnzyme, 'getDomTreeOutputFromFile')
             .mockReturnValue(domTreeOutput);
